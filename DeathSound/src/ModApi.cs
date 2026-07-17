e5bf396e1acf5979e83eae88cde060b144c47c30
M3RT1N99 <132858289+M3RT1N99@users.noreply.github.com>
2026-07-17 15:14:57 +0200

    Removed DeathSound.dll and updated ModApi.cs with explosion logic

--- DeathSound/src/ModApi.cs
+++ DeathSound/src/ModApi.cs
@@ -27,11 +27,36 @@ namespace DeathSound
                     return;
 
                 DeathSoundPlayer.Play(killedEntity.GetDebugName());
+
+                // Setzt einen Timer, um nach 2 Sekunden eine Explosion mit Feuerball-Effekt zu spawnen
+                StartCoroutine(ExplodeAfterDelay(killedEntity.transform.position));
             }
             catch (Exception ex)
             {
                 Log.Error("[DeathSound] EntityKilled handler failed: " + ex);
             }
+        }
+
+        private static IEnumerator ExplodeAfterDelay(Vector3 position)
+        {
+            yield return new WaitForSeconds(2f);
+
+            // Spawnt die Explosion mit einem Feuerball-Effekt, Hauptradius 50m, Außerradius 150m, Hauptschaden 95%, Außerschaden 50%
+            FireBallExplosion.SpawnFireBallExplosion(position, 50f, 150f, 95f, 50f);
+        }
+    }
+
+    public class FireBallExplosion : MonoBehaviour
+    {
+        public static void SpawnFireBallExplosion(Vector3 position, float mainRadius, float outerRadius, float mainDamage, float outerDamage)
+        {
+            // Code zur Auslösung des Feuerball-Effekts und des Schadens
+            // Beispiel:
+            GameObject fireBall = Instantiate(fireBallPrefab, position, Quaternion.identity);
+            fireBall.GetComponent<FireBall>().MainRadius = mainRadius;
+            fireBall.GetComponent<FireBall>().OuterRadius = outerRadius;
+            fireBall.GetComponent<FireBall>().MainDamage = mainDamage;
+            fireBall.GetComponent<FireBall>().OuterDamage = outerDamage;
         }
     }
 }
